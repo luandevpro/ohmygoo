cf303d7f047c7520b079e86719a383668c43c0c7
luandevpro <luandevpro@gmail.com>
2020-11-02 20:40:37 +0700

    v1.0.1

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 #!/usr/bin/env node
-console.log('Hello World');
+const pkg = require('./package.json');
+const chalk = require('chalk');
+
+console.log(`
+  name: ${pkg.name},
+  version: ${pkg.version},
+  description: ${pkg.description},
+`);
+
+console.log(chalk.green('Greetings, Professor Falken.'));
+console.log(chalk.bgBlue.cyanBright('This will look pretty ugly!'));
+console.warn(
+  chalk.yellow(
+    `Here's a yellow line, ${chalk.bold.blue.underline(
+      'with a segment with underlined blue text'
+    )} and then back to yellow.`
+  )
+);
